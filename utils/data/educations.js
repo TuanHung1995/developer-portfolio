aab517f28b8c0e32f0bb808d6b3a40204fc1a4fb
TuanHung1995 <tuanhungg19952gmail.com>
2025-09-03 23:03:27 +0700

    update education infor

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -4,5 +4,11 @@ export const educations = [
     title: "Bachelor Degree",
     duration: "2023 - 2025",
     institution: "Information Technology College",
+  },
+  {
+    id: 2,
+    title: "Java Developer Certificate",
+    duration: "2023 - 2024",
+    institution: "FUNiX",
   }
 ]
