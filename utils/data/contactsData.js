83fdc1dba10cf384575f39c3c0033eb1b1b8ce07
TuanHung1995 <tuanhungg19952gmail.com>
2025-09-03 22:59:51 +0700

    update contacts and eudcation information

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,8 +1,8 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
+    email: 'tuanhungg1995@gmail.com',
+    phone: '+84981516775',
+    address: 'Ho Chi Minh City, Vietnam',
+    github: 'https://github.com/TuanHung1995',
     facebook: 'https://www.facebook.com/abusaid.riyaz/',
     linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
     twitter: 'https://twitter.com/said7388',
